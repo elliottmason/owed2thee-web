0094b7cc31d4be10b6d2c75d45fd8a7fd8579370
R. Elliott Mason <eleo@fastmail.fm>
2015-06-25 15:31:56 -0400

    Renames publishable association to payable

--- app/models/payment_transition.rb
+++ app/models/payment_transition.rb
@@ -1,5 +1,5 @@
 class PaymentTransition < Transition
-  belongs_to :publishable,  foreign_key:  'transitional_id',
-                            foreign_type: 'transitional_type',
-                            polymorphic:  true
+  belongs_to :payable,  foreign_key:  'transitional_id',
+                        foreign_type: 'transitional_type',
+                        polymorphic:  true
 end
