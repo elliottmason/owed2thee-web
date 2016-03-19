0f78b67dae5578bd2bf9d69db701d81ba403cd7f
R. Elliott Mason <eleo@fastmail.fm>
2016-03-19 18:54:50 -0400

    Paginates activity feed

--- app/controllers/users/loans_controller.rb
+++ app/controllers/users/loans_controller.rb
@@ -1,21 +1,46 @@
 module Users
   class LoansController < BaseController
+    ACTIVITIES_PER_PAGE = 10
+    LOANS_PER_PAGE      = 10
+
     def index
       authorize(:Activity)
       authorize(Loan)
 
-      loans = policy_scope(Loan).page(index_params.page)
-      @loans = GroupRecordsByCreationDate.with(loans)
-
-      activities = policy_scope(:Activity) \
-                   .page(index_params.page)
-      @activities = GroupRecordsByCreationDate.with(activities)
+      retrieve_grouped_activities
+      retrieve_grouped_loans
     end
 
     private
 
+    def group_records(records)
+      GroupRecordsByCreationDate.with(records)
+    end
+
     def index_params
       @index_params ||= LoansParams.new(params)
+    end
+
+    def retrieve_activities
+      policy_scope(:Activity).
+        page(index_params.page).
+        per(ACTIVITIES_PER_PAGE)
+    end
+
+    def retrieve_grouped_activities
+      @activities         = retrieve_activities
+      @grouped_activities = group_records(@activities)
+    end
+
+    def retrieve_grouped_loans
+      @loans          = retrieve_loans
+      @grouped_loans  = group_records(@loans)
+    end
+
+    def retrieve_loans
+      policy_scope(Loan).
+        page(index_params.page).
+        per(LOANS_PER_PAGE)
     end
   end
 end
