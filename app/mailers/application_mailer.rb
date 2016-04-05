6c3eea968694217b01804e1649203399eff48baf
R. Elliott Mason <eleo@fastmail.fm>
2016-04-05 00:53:14 -0400

    Shows app title as email sender

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,9 +1,19 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'postmaster@' + Rails.application.secrets.domain_name
+  class << self
+    def app_title
+      I18n.t('app.title')
+    end
+
+    def domain_name
+      Rails.application.secrets.domain_name
+    end
+  end
+
+  default from: "#{app_title} <notifications@#{domain_name}>"
 
   private
 
   def app_title
-    I18n.t('app.title')
+    self.class.app_title
   end
 end
