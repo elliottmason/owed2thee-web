002fbe70482c804324006edea0482c3c02932d69
R. Elliott Mason <eleo@fastmail.fm>
2016-06-19 16:37:08 -0400

    Redirects root to loans#index when signed in

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -3,6 +3,8 @@ class VisitorsController < ApplicationController
   skip_after_action :verify_policy_scoped
 
   def index
+    redirect_to(:loans) && return if user_signed_in?
+
     @loan = LoanForm.new
   end
 end
