8ce638ff009431b65cbe6227a3db87ea2cc1d39f
R. Elliott Mason <eleo@fastmail.fm>
2018-03-08 10:35:42 -0500

    Fixes all deprecation warnings

--- spec/support/devise.rb
+++ spec/support/devise.rb
@@ -1,7 +1,7 @@
 RSpec.configure do |config|
   config.include Warden::Test::Helpers
-  config.include Devise::TestHelpers, type: :controller
-  config.include Devise::TestHelpers, type: :view
+  config.include Devise::Test::ControllerHelpers, type: :controller
+  config.include Devise::Test::ControllerHelpers, type: :view
 
   config.before :each do
     Warden.test_mode!
